310b9acd731bc7fe3e6a6fc5c6499646e86aaf1b
Dan Finlay <dan@danfinlay.com>
2016-02-27 22:22:14 -0800

    Basic facebook auth working with static templates

--- api/app/models/user.rb
+++ api/app/models/user.rb
@@ -1,2 +1,13 @@
-class User < ApplicationRecord
+class User < ActiveRecord::Base
+  def self.from_omniauth(auth)
+    binding.pry
+    where(auth.slice(:provider, :uid).as_json).first_or_initialize.tap do |user|
+      user.provider = auth.provider
+      user.uid = auth.uid
+      user.name = auth.info.name
+      user.oauth_token = auth.credentials.token
+      user.oauth_expires_at = Time.at(auth.credentials.expires_at)
+      user.save!
+    end
+  end
 end
--- api/config/routes.rb
+++ api/config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'sessions/create'
 
-  # Serve websocket cable requests in-process
-  # mount ActionCable.server => '/cable'
+  get 'sessions/destroy'
+
+  get 'home/show'
+    get 'auth/:provider/callback', to: 'sessions#create'
+    get 'auth/failure', to: redirect('/')
+    get 'signout', to: 'sessions#destroy', as: 'signout'
+
+    resources :sessions, only: [:create, :destroy]
+    resource :home, only: [:show]
+
+    root to: "home#show"
 end
